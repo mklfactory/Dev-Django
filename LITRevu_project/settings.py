3018ccd824aee6a84e0b981debea857cdb09db68
mklfactory <mklfactory@gmail.com>
2026-06-12 11:51:10 +0200

    updates settings.py

--- LITRevu_project/settings.py
+++ LITRevu_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+TAILWIND_APP_NAME = 'theme'
