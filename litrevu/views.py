bdbc2b4621a333841037da61a8663edb5beed338
mklfactory <mklfactory@gmail.com>
2026-06-12 14:39:27 +0200

    update views.py

--- litrevu/views.py
+++ litrevu/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        return redirect('feed')
+    return redirect('login')
 
+@login_required
 def feed(request):
     return render(request, 'feed.html')
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('feed')
+    
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('feed')
+    
+    return render(request, 'login.html', {'form': form})
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('feed')
+    
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('feed')
+    
+    return render(request, 'register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
